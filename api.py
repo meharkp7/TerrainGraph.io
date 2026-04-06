2459b0a9ed47c974485e772f7f86b34d2d7c5ed0
meharkp7 <meharkapoor7@gmail.com>
2026-04-07 01:07:53 +0530

    api.py updated cors

--- api.py
+++ api.py
@@ -25,6 +25,7 @@ app = FastAPI(title="Terrain Intelligence API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
